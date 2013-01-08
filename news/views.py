b50f7da83619909119cccfa5585ffd86b6c46944
Jorgen Eri <jfemail3@gmail.com>
2013-01-08 15:42:29 +0000

    new content model

--- news/views.py
+++ news/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render_to_response, get_object_or_404
 from news.models import Newspost
 from django.template import RequestContext
+from content.models import Slide
 
 def index(request):
     newspost_list = Newspost.objects.all().order_by('-pub_date')[:5]
+    slide_list = Slide.objects.all()
     return render_to_response(
         'news/index.html', 
-        {'newspost_list': newspost_list},
+        {'newspost_list': newspost_list, 'slide_list': slide_list},
         context_instance=RequestContext(request))
 
 
