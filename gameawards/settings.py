5f085597fc1ac01b4457c3dc56cc0d24de8a0289
Jorgen Eri <jfemail3@gmail.com>
2013-01-20 20:39:58 +0000

    settings for test prod

--- gameawards/settings.py
+++ gameawards/settings.py
@@ -198,7 +198,7 @@ else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-            'NAME': 'gameawards',    # Or path to database file if using sqlite3.
+            'NAME': 'gameawards2',    # Or path to database file if using sqlite3.
             'USER': 'gameawards',                      # Not used with sqlite3.
             'PASSWORD': 'greenballs',                  # Not used with sqlite3.
             'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -237,18 +237,18 @@ else:
 
     # Absolute filesystem path to the directory that will hold user-uploaded files.
     # Example: "/home/media/media.lawrence.com/media/"
-    MEDIA_ROOT = '/home/gameawards/webapps/django/gameawards/assets'
+    MEDIA_ROOT = '/home/gameawards/webapps/gamedia/'
 
     # URL that handles the media served from MEDIA_ROOT. Make sure to use a
     # trailing slash.
     # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-    MEDIA_URL = '/assets/'
+    MEDIA_URL = '/media/'
 
     # Absolute path to the directory static files should be collected to.
     # Don't put anything in this directory yourself; store your static files
     # in apps' "static/" subdirectories and in STATICFILES_DIRS.
     # Example: "/home/media/media.lawrence.com/static/"
-    STATIC_ROOT = '/home/gameawards/webapps/gastatic/'
+    STATIC_ROOT = '/home/gameawards/webapps/gastatic2/'
 
     # URL prefix for static files.
     # Example: "http://media.lawrence.com/static/"
@@ -256,7 +256,7 @@ else:
 
     # Additional locations of static files
     STATICFILES_DIRS = (
-        "/home/gameawards/webapps/django/gameawards/static",
+        "/home/gameawards/webapps/django2/gameawards2/static",
         # Put strings here, like "/home/html/static" or "C:/www/django/static".
         # Always use forward slashes, even on Windows.
         # Don't forget to use absolute paths, not relative paths.
@@ -299,7 +299,7 @@ else:
         # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
         # Always use forward slashes, even on Windows.
         # Don't forget to use absolute paths, not relative paths.
-        '/home/gameawards/webapps/django/gameawards/templates'
+        '/home/gameawards/webapps/django2/gameawards2/templates'
     )
 
     ACCOUNT_ACTIVATION_DAYS = 7
