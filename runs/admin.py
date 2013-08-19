e191453ccd071337c6d9cabc1a42a3d23798525f
jorgeer <jfemail3@gmail.com>
2013-08-19 15:42:17 +0000

    added admin upload

--- runs/admin.py
+++ runs/admin.py
@@ -1,7 +1,7 @@
-from runs.models import Run, Developer, Game
+from runs.models import Run, Developer, Game, Upload
 from django.contrib import admin
 
-
+admin.site.register(Upload)
 admin.site.register(Game)
 admin.site.register(Run)
 admin.site.register(Developer)
