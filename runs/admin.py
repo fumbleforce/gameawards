0fae9482c2f55af0d05ca4359b5d074411326213
Jorgen Eri <jfemail3@gmail.com>
2013-08-19 15:51:52 +0000

    upload admin

--- runs/admin.py
+++ runs/admin.py
@@ -1,7 +1,18 @@
 from runs.models import Run, Developer, Game, Upload
 from django.contrib import admin
 
-admin.site.register(Upload)
 admin.site.register(Game)
 admin.site.register(Run)
 admin.site.register(Developer)
+
+
+
+
+class RunsAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,                  {'fields': ['title']}),
+        ('Content',               {'fields': ['version', 'game', 'uploaded_file']}),
+    ]
+    list_display = ('title','version', 'game', 'uploaded_file')
+    
+admin.site.register(Upload, RunsAdmin)
