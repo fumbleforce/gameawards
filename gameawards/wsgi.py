ec605f46e2167b216ba4e314439ed5d662c98a5c
jorgeer <jfemail3@gmail.com>
2014-04-09 14:22:37 +0200

    apache conf to prod

--- gameawards/wsgi.py
+++ gameawards/wsgi.py
@@ -15,7 +15,7 @@ framework.
 """
 import os
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gameawards.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gameawards.settings.prod")
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
